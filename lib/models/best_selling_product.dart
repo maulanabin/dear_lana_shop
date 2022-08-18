db9b7310b5790b335c9bde7df27566752778f669
maulanabin <2041720132@student.polinema.ac.id>
2022-08-18 10:03:44 +0700

    Menambahkan carousel produk dan produk yang terlaris

---
+++ lib/models/best_selling_product.dart
@@ -0,0 +1,81 @@
+class BestSellingProduct {
+  String name;
+  String price;
+  String image;
+  String unit;
+  String description;
+
+  BestSellingProduct(
+      {required this.name,
+      required this.price,
+      required this.image,
+      required this.unit,
+      required this.description});
+}
+
+var bestSellingProductList = [
+  BestSellingProduct(
+      name: "GARNIER - Serum Day Cream",
+      price: " 60.000",
+      image: "../images/garnier.jpg",
+      unit: "400 gram",
+      description:
+          "Ekstrak alami sakura dan Pore Smoothing Serum di dalamnya efektif mampu mencerahkan wajah kusam, sekaligus membuat pori-pori di permukaan tampak halus. Teknologi Talc Touch, melembabkan tanpa membuat wajah mengkilap akibat minyak berlebih. Mengandung SPF21/PA+++nya melindungi dari sinar UV agar kulit tak kembali gelap."),
+  BestSellingProduct(
+      name: "EMINA - Cheeklit Cream Blush",
+      price: " 30.000",
+      image: "../images/cheek.jpg",
+      unit: "50 gram",
+      description:
+          "Cheek Lit Cream Blush adalah blush on dengan tekstur krim yang sangat mudah untuk diaplikasikan pada wajah !"),
+  BestSellingProduct(
+      name: "VASELINE - Lip Theraphy Jar",
+      price: " 50.000",
+      image: "../images/lip.jpg",
+      unit: "7 gram",
+      description:
+          "Vaseline Lip Therapy dari USA dengan kandungan Petroleum Jelly yang memberikan kelembaban pada bibir. Gunakan setiap hari untuk mengurangi bibir kering dan pecah-pecah serta menjaga kesehatan bibir. Simpan ditempat sejuk dan hindari dari paparan matahari langsung."),
+  BestSellingProduct(
+      name: "EMINA - Beauty Bliss BB Cream",
+      price: " 30.000",
+      image: "../images/bliss.jpg",
+      unit: "20 gram",
+      description:
+          "Beauty Bliss BB Cream yang merupakan kombinasi unik dari pelembab, primer, antioksidan dan foundation memberikan banyak manfaat dalam satu olesan."),
+  BestSellingProduct(
+      name: "AZARINE - Hydrashoothe Sunscreen Gel Spf45+++",
+      price: " 55.000",
+      image: "../images/azarine.png",
+      unit: "100 gram",
+      description:
+          "Tabir surya wajah dalam bentuk gel (water base) yang sangat ringan, dingin dan mudah meresap untuk seluruh jenis kulit termasuk kulit berminyak dan acne prone skin. Di formulasikan dengan kandungan bahan alami Propolis, aloe Vera, green tea dan delima untuk melindungi kulit dari sinar UV A & UV B serta menutrisi kulit."),
+  BestSellingProduct(
+      name: "AVOSKIN - YOUR SKIN BAE SERIES Niacinamide 12% + Centella",
+      price: " 125.000",
+      image: "../images/avoskin.png",
+      unit: "145 gram",
+      description:
+          "Brightening dan hydrating serum terbaru dari Avoskin dengan Niacinamide 12% yang berfungsi optimal sebagai booster untuk mencerahkan kulit, memudarkan dark-spots, anti-acne, anti-aging, dan memiliki  melembapkan kulit."),
+  BestSellingProduct(
+      name: "MOTHER OF PEARL - Microblur Translucent Loose Powder",
+      price: " 150.000",
+      image: "../images/pearl.png",
+      unit: "10 gram",
+      description:
+          "Kunci makeup dan samarkan tampilan pori-porimu dengan Microblur Translucent Loose Powder ini. Cocok untuk semua jenis kulit, tidak mengubah warna kulitmu dan tidak meninggalkan residu putih yang membuat kulitmu tampak berwarna abu-abu."),
+  BestSellingProduct(
+      name:
+          "LOVE BEAUTY AND PLANET - Soothe & Serene Argan Oil & Lavender Body Lotion",
+      price: " 80.000",
+      image: "../images/love.png",
+      unit: "650 gram",
+      description:
+          "Dengan kandungan Golden Argan Oil dan keharuman French Lavender, Soothe and Serene body lotion melembapkan, melembutkan dan menenangkan kulit. Body Lotion ini dikemas dalam 100% botol daur ulang, vegan-certified, cruelty free, bebas dari pewarna, silikon, dan paraben."),
+  BestSellingProduct(
+      name: "EMINA - Bare With Me Mineral Loose Powder",
+      price: " 40.000",
+      image: "../images/emina1.jpg",
+      unit: "20 gram",
+      description:
+          "Salah satu bedak untuk kulit berminyak produksi lokal adalah Emina Bare With Me Mineral Loose Powder yaitu bedak tabur yang memiliki kandungan Lauroyl lisine, mica dan oil absorbing powder yang bekerja secara sinergis untuk memberikan hasil yang halus, natural, dan bebas kilap sepanjang hari."),
+];
---
+++ lib/models/caraousel_banner.dart
@@ -0,0 +1,17 @@
+class CarouselBanner {
+  String title;
+  String image;
+
+  CarouselBanner({required this.title, required this.image});
+}
+
+var CarouselBannerList = [
+  CarouselBanner(
+      title: "Skincare untuk kesehatan kulitmu hanya ada di Dear Lana ❤",
+      image: "../images/model3.jpg"),
+  CarouselBanner(title: "Skincare Products ❤", image: "../images/products.jpg"),
+  CarouselBanner(title: "Makeup & Skincare ❤", image: "../images/model2.jpg"),
+  CarouselBanner(
+      title: "Suncreen and water are your skins best friends ❤",
+      image: "../images/model4.jpg"),
+];
